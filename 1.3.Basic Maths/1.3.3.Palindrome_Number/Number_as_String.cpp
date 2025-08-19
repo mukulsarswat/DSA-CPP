4924ef4a4fd7b94abdedef46cceab8c6990999b5
mukulsarswat <137614804+mukulsarswat@users.noreply.github.com>
2025-08-19 18:10:12 +0530

    Create Number_as_String.cpp

---
+++ 1.3.Basic Maths/1.3.3.Palindrome_Number/Number_as_String.cpp
@@ -0,0 +1,29 @@
+//  GFG Solution
+class Solution {
+  public:
+    bool isPalindrome(int n) {
+       string s = to_string(abs(n));
+	int len = s.length();
+
+    for (int i = 0; i < len / 2; i++) {
+        if (s[i] != s[len - i - 1])
+            return false;
+    }
+    return true;
+    }
+};
+// LeetCode Solution
+class Solution {
+  public:
+    bool isPalindrome(int n) {
+        if(n<0) return false;
+       string s = to_string(abs(n));
+	int len = s.length();
+
+    for (int i = 0; i < len / 2; i++) {
+        if (s[i] != s[len - i - 1])
+            return false;
+    }
+    return true;
+    }
+};
