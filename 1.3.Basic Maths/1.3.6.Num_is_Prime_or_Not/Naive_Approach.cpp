9d02a28d543deb08ae97244b9dd8bfc9fb0bf574
mukulsarswat <137614804+mukulsarswat@users.noreply.github.com>
2025-08-20 20:49:16 +0530

    Create Naive_Approach.cpp
    Naive_Approach

---
+++ 1.3.Basic Maths/1.3.6.Num_is_Prime_or_Not/Naive_Approach.cpp
@@ -0,0 +1,15 @@
+//GFG Solution
+class Solution{
+ public:
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+    
+        for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+    
+        return true;
+    }
+};
